5e29518a596d826fc55b8fac6383db58e0216832
Levon <levonroberts28@gmail.com>
2026-01-23 10:23:25 +0000

    Create services namespace with IAudioMixerService and WindowsAudioMixerService. IAudioMixerService implemented to deal with the interface of what the service can do

---
+++ Services/IAudioMixerService.cs
@@ -0,0 +1,18 @@
+﻿using QuickMixer.Models;
+using System.Collections.Generic;
+
+
+namespace SoundBar.Services
+{
+    public interface IAudioMixerService
+    {
+        // Returns list of all apps currently making sound
+        List<AudioAppModel> GetActiveAudioSessions();
+
+        // Update volume for specific app using PID
+        void SetVolume(int processID, float level);
+
+        // Mutes or unmutes specific app
+        void SetMute(int processID, bool isMuted);
+    }
+}
--- obj/Debug/net10.0-windows/SoundBar.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/SoundBar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoundBar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6390f678dcb34f048a45960a6d6186cc60286e84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7327467c0676857f99da0e4c25003b50f5b09b77")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoundBar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoundBar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
